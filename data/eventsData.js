a59142792d2a8afaef92ae6234adcd8987d13909
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-24 19:36:12 +0530

    Updated Events page

--- data/eventsData.js
+++ data/eventsData.js
@@ -8,12 +8,31 @@ const eventsData = [
     href: 'https://www.instagram.com/p/CuKCeQTt69m/',
   },
   {
-    title: 'Enigma',
-    description: `The first of its kind, ACM NIT Surat brings to you an open source contribution event, 
-    paired with a hands-on Git/Github workshop. Participants will be able to experience the thrill of contributing
+    title: 'Open Source Workshop',
+    description: `ACM NIT Surat brings to you an open source and hands-on introduction to Git/Github workshop. Participants will be able to experience the thrill of contributing
     to real-world open source projects, putting them in a good position for Hacktober Fest 2023`,
-    imgSrc: '/static/images/openSource.jfif',
-    href: 'https://www.instagram.com/p/Cij7J9GNyFD',
+    imgSrc: '/static/images/openSource.jpg',
+    href: 'https://www.instagram.com/p/Cx2043MtAf1/',
+  },
+  {
+    title: 'SIH 2023 Stage 1: Ideathon',
+    description: `𝗦𝗺𝗮𝗿𝘁 𝗜𝗻𝗱𝗶𝗮 𝗛𝗮𝗰𝗸𝗮𝘁𝗵𝗼𝗻 𝟮𝟬𝟮𝟯 💡 is here, this nationwide initiative is all about fostering a culture of product innovation and nurturing a problem-solving 🧩mindset among our young talents. 𝗔𝗖𝗠 and 𝗚𝗗𝗦𝗖 are thrilled to kickstart SIH 2023 with the first stage: 𝗧𝗵𝗲 𝗦𝗜𝗛 𝗜𝗱𝗲𝗮𝘁𝗵𝗼𝗻.💫
+`,
+    imgSrc: '/static/images/SIH.jpg',
+    href: 'https://www.instagram.com/p/CxC21fZJXdC/',
+  },
+  {
+    title: 'Inception 8.0',
+    description: `Put on your thinking caps! For Inception 8.0 is finally here! 🖥️ Are you ready to display your coding prowess among your peers? If so, you don’t want to miss this one ✨ Join us in solving the most fun and challenging DSA problems which are set to help you prepare for your internship interviews as well.`,
+    imgSrc: '/static/images/inception8.jpg',
+    href: 'https://www.instagram.com/p/CyvgaQ1t0Qo/',
+  },
+  {
+    title: 'Dotslash 7.0',
+    description: `Mark your calendars! 📆 Excitement is building up as we announce the dates for Dotslash 7.0, our prestigious hackathon set to unfold on the NIT Surat campus on January 27-28. “🚀
+Gear up for a weekend of innovation, collaboration, and cutting-edge solutions! 💡 💻`,
+    imgSrc: '/static/images/dotslash7.jpg',
+    href: 'https://www.instagram.com/p/C0oI4FRtF_0/',
   },
 ]
 
